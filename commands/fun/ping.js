a35b3773cfdb193505e7f780a010d7c4830fbfc2
13-Rob <rcgr.13@gmail.com>
2023-06-20 10:56:11 -0700

    Command handling and folders

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -2,8 +2,8 @@
 // use interaction.client to access client instance
 const { SlashCommandBuilder } = require('discord.js');
 
-// A command need at least a name and a description
-// module.exports allows this class to be found by other files
+// A command need at least a name and a description module.exports allows this class to be found by
+// other files
 module.exports =  {
     // data property provides the command definition for registering to Discord
     data: new SlashCommandBuilder()
@@ -11,8 +11,7 @@ module.exports =  {
         .setDescription('Replies with Pong!'),
     // execute method contains the functionaliy of the command
     async execute(interaction) {
-        // Interaction method confirms to Discord that the
-        // bot successfully received an interaction
+        // Interaction method confirms to Discord that the bot successfully received an interaction
         await interaction.reply('Pong!');
     },
 };
--- index.js
+++ index.js
@@ -1,14 +1,72 @@
+// Node's native file system module
+const fs = require('node:fs');
+
+// Node's native path utility module
+const path = require('node:path');
+
 // Require necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
 // New client instance
 // Guilds refers to Discord Servers
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Used to store and efficiently retrieve commands
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the collection
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute property`);
+        }
+    }
+}
+
 // When client is ready, run once
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Listener for slash commands (interactions)
+client.on(Events.InteractionCreate, async interaction => {
+    // Not all interactions are ChatInputCommands
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command!',
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                ephemeral: true,
+            })
+        }
+    }
+
+    console.log(interaction);
 });
 
 // Log in to Discord
